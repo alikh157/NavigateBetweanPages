b061b3027672160f202cb26d62b6fb4d94626926
alikh157 <ali.khakbaz134878@gmail.com>
2019-08-17 17:12:07 +0430

    omdb

---
+++ app/src/main/java/com/example/navigatebetweanpages/TestRecycler.java
@@ -0,0 +1,14 @@
+package com.example.navigatebetweanpages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestRecycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_recycler);
+    }
+}
---
+++ app/src/main/java/com/example/navigatebetweanpages/testRecyclerAdapter.java
@@ -0,0 +1,4 @@
+package com.example.navigatebetweanpages;
+
+public class testRecyclerAdapter {
+}
