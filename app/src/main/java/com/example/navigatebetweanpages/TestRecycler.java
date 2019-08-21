cf84c9e610bd3f98dcd43445a7bc7f3e1ff47dba
alikh157 <ali.khakbaz134878@gmail.com>
2019-08-21 12:17:48 +0430

    not complete

--- app/src/main/java/com/example/navigatebetweanpages/TestRecycler.java
+++ app/src/main/java/com/example/navigatebetweanpages/TestRecycler.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.icu.text.CaseMap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -34,50 +36,89 @@ public class TestRecycler extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_recycler);
 
-
         ////////////////////////////////////////////////////////////////////////////////////
-        final TextView request=findViewById(R.id.request);
-        final Button btnSrc=findViewById(R.id.btnSrc);
+        final TextView request = findViewById(R.id.request);
+        final Button btnSrc = findViewById(R.id.btnSrc);
 
         btnSrc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-        String request1=request.getText().toString();
-        String address="http://www.omdbapi.com/?s="+request1+"&apikey=70ad462a";
+                String request1 = request.getText().toString();
+                String address = "http://www.omdbapi.com/?s=" + request1 + "&apikey=70ad462a";
                 System.out.println(address);
-        AsyncHttpClient client=new AsyncHttpClient();
-        client.get(address,new JsonHttpResponseHandler(){
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                super.onSuccess(statusCode, headers, response);
-                System.out.println(response.toString());
-                try {
-                    JSONObject object=new JSONObject(response.toString());
-                    String result=object.getString("Search");
-                    JSONArray jsonarray = new JSONArray(result);
-                      List<String> list=new ArrayList<String>();
-                    for (int i = 0; i < jsonarray.length(); i++) {
-                        JSONObject jsonobject = jsonarray.getJSONObject(i);
-                        String Title = jsonobject.getString("Title");
-                        list.add(Title);
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get(address, new JsonHttpResponseHandler() {
+                Button btnSave = findViewById(R.id.btnSave);
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        super.onSuccess(statusCode, headers, response);
+                        System.out.println(response.toString());
+                        try {
+                            JSONObject object = new JSONObject(response.toString());
+                            String result = object.getString("Search");
+                            JSONArray jsonarray = new JSONArray(result);
+                            final List<String> list = new ArrayList<String>();
+                            for (int i = 0; i < jsonarray.length(); i++) {
+                                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                                String Title = jsonobject.getString("Title");
+                                list.add(Title);
+                                final String list1=list.get(i).toString();
+                                btnSave.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        OpenDBHelper db1=new OpenDBHelper(this);
+                                        db1.insert(list1);
+                                    }
+                                });
+                            }
+                            RecyclerView recycler = findViewById(R.id.recycler);
+                            testRecyclerAdapter adapter = new testRecyclerAdapter(list);
+                            recycler.setAdapter(adapter);
+                            recycler.setLayoutManager(new LinearLayoutManager(TestRecycler.this, RecyclerView.VERTICAL, false));
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
                     }
-        RecyclerView recycler=findViewById(R.id.recycler);
-        testRecyclerAdapter adapter=new testRecyclerAdapter(list);
-        recycler.setAdapter(adapter);
-        recycler.setLayoutManager(new LinearLayoutManager(TestRecycler.this,RecyclerView.VERTICAL,false));
-                }catch (Exception e){
-                    System.out.println(e);
-                }
-            }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-            }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        super.onFailure(statusCode, headers, throwable, errorResponse);
+                    }
 
-        });
+                });
             }
         });
+        //--------------------------------------------------------
 
+    }
+
+}
+class OpenDBHelper extends SQLiteOpenHelper {
+    String db_create_query = "" +
+            "CREATE TABLE film (" +
+            " _id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            " name TEXT " + ")" +
+            "";
+     SQLiteDatabase db;
+
+    public OpenDBHelper(View.OnClickListener context) {
+        super((Context) context,"db" , null, 1);
+        db = getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db ) {
+        db.execSQL(db_create_query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public void insert(String name){
+        String insert="INSERT INTO film ("+name+")VALUES( '"+name+"')";
+        db = getWritableDatabase();
+        db.execSQL(insert);
+        db.close();
     }
 }
